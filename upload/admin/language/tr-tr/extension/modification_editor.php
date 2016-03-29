57925070de0188f87d2dc0aa6c19dac855ffe838
changerrr <cihangire@gmail.com>
2016-03-29 15:20:15 +0300

    Create modification_editor.php

---
+++ upload/admin/language/tr-tr/extension/modification_editor.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']      = 'Modifikasyon Editor';
+
+// Text
+$_['text_success_edit']  = 'Başarılı: Modifikasyon güncellendi!';
+$_['text_success_add']   = 'Başarılı: Modifikasyon eklendi!';
+$_['text_clear_data']    = 'Başarılı: Veritabanı önbelleği temizlendi!';
+$_['text_clear_image']   = 'Başarılı: Resim önbelleği temizlendi!';
+$_['text_loading']       = 'Kaydediliyor...';
+$_['text_erasing']       = 'Siliniyor...';
+$_['text_modifications'] = 'Modifikasyonlar';
+$_['text_new']           = 'Yeni Modifikasyon';
+
+// Entry
+$_['entry_xml_code']     = 'XML Kod';
+
+// Button
+$_['button_new']         = 'Yeni Modifikasyon';
+$_['button_edit']        = 'Modifikasyon Düzenleme';
+$_['button_download']    = 'XML İndir';
+$_['button_clear_data']  = 'Veritabanı önbelleğini temizle';
+$_['button_clear_image'] = 'Resim önbelleğini temizle';
+$_['button_save']        = 'Modifikasyonu kaydet';
+$_['button_return']      = 'Modifikasyonlara Dön';
+
+// Error
+$_['error_permission']   = 'Uyarı: "Modifikasyonlar" için değiştirme yetkiniz yok!';
+$_['error_code']         = 'Modifikasyon için benzersiz "Code" tanımlanmalıdır!';
+$_['error_exists']       = 'Modifikasyon - %s için tanımlanan "Code" kullanılmaktadır!';
+$_['error_modification'] = 'Modifikasyon ID geçerli değil!';
+$_['error_file']         = 'XML dosyası bulunamadı!';
+$_['error_headers']      = 'Hata: "Headers" zaten gönderildi!';
